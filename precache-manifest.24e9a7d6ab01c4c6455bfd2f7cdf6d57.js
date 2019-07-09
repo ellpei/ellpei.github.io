62ed414d4d436346459e7e8f195c17a2efa563d3
loulou <loulou.pei@hotmail.com>
2019-07-09 16:57:54 +0200

    Updates

---
+++ precache-manifest.24e9a7d6ab01c4c6455bfd2f7cdf6d57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a65ba5b4baaaa60a7b792d01e81486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbd32893a600a0f56c32",
+    "url": "/static/css/main.96dc75db.chunk.css"
+  },
+  {
+    "revision": "33c1b2d0e9aaef92a009",
+    "url": "/static/js/2.89451e4a.chunk.js"
+  },
+  {
+    "revision": "cbd32893a600a0f56c32",
+    "url": "/static/js/main.14aa3c6e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
