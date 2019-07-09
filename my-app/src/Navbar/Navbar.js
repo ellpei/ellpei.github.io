712502be1af7bd44d5a818946bce5205ce9157fb
loulou <loulou.pei@hotmail.com>
2019-07-10 00:09:24 +0200

    animation

--- my-app/src/Navbar/Navbar.js
+++ my-app/src/Navbar/Navbar.js
@@ -6,6 +6,32 @@ class Navbar extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      prevScrollpos: window.pageYOffset,
+      visible: true
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+
+  handleScroll = () => {
+    const { prevScrollpos } = this.state;
+
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollpos > currentScrollPos;
+
+    this.setState({
+      prevScrollpos: currentScrollPos,
+      visible: visible
+    });
   }
 
   scrollToTop = () => {
@@ -14,17 +40,19 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="Navbar" id="navbar">
+      <div className="Navbar">
+      <nav className={ (this.state.visible ? "top-navbar" : "top-navbar-hidden")} id="navbar">
         <div className="nav-content">
           <ul className="nav-items">
             <li className="nav-item"><Link activeClass="active" to='home' spy={true} smooth={true} offset={-70} duration={500}>Home</Link></li>
             <li className="nav-item"><Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500}>About</Link></li>
             <li className="nav-item"><Link activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500}>Contact</Link></li>
           </ul>
-          <div className="go-top-btn" onClick={this.scrollToTop}>&#8963;</div>
 
         </div>
       </nav>
+      <div className="go-top-btn" onClick={this.scrollToTop}>&#8963;</div>
+      </div>
     );
   }
 
